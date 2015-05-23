1109a0752fb7fefd6a1f5f87945bc540c5b7d6f0
Jazz Kersell <jazzkersell@gmail.com>
2015-05-22 20:38:02 -0400

    initial inplementation of push and pop.

--- stack/stack.c
+++ stack/stack.c
@@ -5,23 +5,36 @@
 
 typedef struct Stack {
     char arr[INIT_SIZE];
+    int size;
 } Stack;
 
-void initStack(Stack* stack) {
+void stack_init(Stack* stack) {
     memset(stack->arr, 0, INIT_SIZE);
+    stack->size = 0;
+}
+
+void stack_push(Stack* stack, char value) {
+    stack->arr[stack->size] = value;
+    stack->size++;
+}
+
+char stack_pop(Stack* stack) {
+    stack->size--;
+    return stack->arr[stack->size];
 }
 
 int main(int argc, char* argv[]) {
     FILE* fp = fopen(argv[1], "r");
 
     Stack myStack;
-    initStack(&myStack);
+    stack_init(&myStack);
 
     if (fp != NULL) {
         int c = fgetc(fp);
         while (c != EOF) {
-            printf("%c", c);
+            stack_push(&myStack, c);
             c = fgetc(fp);
+            printf("%c", stack_pop(&myStack));
         }
     }
 
